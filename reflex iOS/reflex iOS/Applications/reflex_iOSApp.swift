ace6d5ab0052a0be231544041a9df6b6a822d309
Pierre <Pierre_gourgouillon@hotmail.com>
2022-07-20 19:20:29 +0200

    Init App

---
+++ reflex iOS/reflex iOS/Applications/reflex_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  reflex_iOSApp.swift
+//  reflex iOS
+//
+//  Created by Pierre Gourgouillon on 20/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct reflex_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
